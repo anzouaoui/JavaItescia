42c38142796407f31941644e7a6919306c984a35
anzouaoui <anzouaoui78@yahoo.fr>
2017-10-23 21:39:13 +0200

    Ajout de l'exercice 5

---
+++ java05/src/java05/Message.java
@@ -0,0 +1,13 @@
+package java05;
+
+public class Message {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		while(i < 8) {
+			System.out.println("Bonjour " + (i+1) + " fois");
+			i++;
+		}
+	}
+}
