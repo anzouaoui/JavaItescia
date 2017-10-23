9e31fe5d4ed156e65cb07e927773aa8d2ba8f3d5
anzouaoui <anzouaoui78@yahoo.fr>
2017-10-23 21:19:05 +0200

    Ajout de l'exercice 2

---
+++ java02/src/java02/Types.java
@@ -0,0 +1,44 @@
+package java02;
+
+public class Types {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/**
+		 * TYPES NUMERIQUES
+		 */
+		//int
+		int nombre1 = 1;
+		System.out.println("int: " + nombre1);
+		//double
+		double nombre2 = 25.0d;
+		System.out.println("double" + nombre2);
+		//float
+		float nombre3 = 15.6f;
+		System.out.println("float" + nombre3);
+		//byte
+		byte nombre4 = 12;
+		System.out.println("byte" + nombre4);
+		//short
+		short nombre5 = 2;
+		System.out.println("short" + nombre5);
+		//long
+		long nombre6 = 127;
+		System.out.println("long: " + nombre6);
+		
+		/**
+		 * TYPE CARACTERE
+		 */
+		//char
+		char caractere = 'A';
+		System.out.println("char: " + caractere);
+		/**
+		 * TYPE BOOLEEN
+		 */
+		//boolean
+		boolean booleen = false;
+		System.out.println("boolean: " + booleen);
+		
+	}
+
+}
