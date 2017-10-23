a50a0e61864126b3ae5299f89dff24aed7f81120
anzouaoui <anzouaoui78@yahoo.fr>
2017-10-23 22:00:53 +0200

    Ajout de l'exercice 7

---
+++ java07/src/java07/Somme50.java
@@ -0,0 +1,14 @@
+package java07;
+
+public class Somme50 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int somme = 0;
+		for (int i=1 ; i<=50 ; i++) {
+			somme += i;
+		}
+		System.out.println("Somme des chiffres de 1 à 50: " + somme);
+	}
+
+}
