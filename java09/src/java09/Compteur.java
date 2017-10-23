2c8f1e6d7f5f51410f5d86b9dbf0e025d7c0dede
anzouaoui <anzouaoui78@yahoo.fr>
2017-10-23 22:28:05 +0200

    Ajout de l'exercice 9

---
+++ java09/src/java09/Compteur.java
@@ -0,0 +1,28 @@
+package java09;
+
+public class Compteur {
+	//Membre privée de la classe Compteur
+	private int cpt;
+	
+	/**
+	 * Fonction permettant d'augmenter le membre cpt  
+	 */
+	public void increment() {
+		cpt++;
+	}
+	
+	/**
+	 * Fonction permettant d'afficher la valeur de cpt
+	 */
+	public void affiche() {
+		System.out.println("Valeur: " + cpt);
+	}
+	
+	/**
+	 * Fonction permettant de lire le compteur
+	 * @return
+	 */
+	public int lireCount() {
+		return cpt;
+	}
+}
