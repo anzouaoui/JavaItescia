4fb7ecfd9ef0b087f1bcb9118171e3ebf84bc254
anzouaoui <anzouaoui78@yahoo.fr>
2017-10-23 20:52:55 +0200

    Ajout de l'exercice 1

--- java01/src/java01/Bonjour.java
+++ java01/src/java01/Bonjour.java
@@ -4,7 +4,6 @@ public class Bonjour {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Bonjour à tous");
+		System.out.println("Bonjour à tous(tes)");
 	}
-
 }
